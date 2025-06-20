455ee1749ceb7ace4ae91c5b1034efc7d0862fb6
miriamsampson <sumayahsampson@gmail.com>
2025-06-20 09:39:55 -0400

    Initial commit

---
+++ KidsQuranRecitation/wwwroot/js/site.js
@@ -0,0 +1,20 @@
+﻿window.addEventListener('load', () => {
+    const title = document.getElementById('title');
+    const bismillah = document.getElementById('bismillah');
+    const videoBox = document.getElementById('videoBox');
+
+    setTimeout(() => {
+        title.style.opacity = '1';
+        title.style.transform = 'translateY(0)';
+    }, 500);
+
+    setTimeout(() => {
+        bismillah.style.opacity = '1';
+        bismillah.style.transform = 'translateY(0)';
+g    }, 1500);
+
+    setTimeout(() => {
+        videoBox.style.opacity = '1';
+        videoBox.style.transform = 'translateY(0)';
+    }, 2500);
+});
